ce39f2a3d4c2d937cba372c9b0b019e59ff0d7c3
jchanda1 <74137050+jchanda1@users.noreply.github.com>
2024-12-12 07:25:06 +0000

    api call in proxy working

--- quotes/index.js
+++ quotes/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ success: "Hello from the quotes app" });
+// router.get("/", (req, res) => {
+//   res.json({ success: "Hello from the quotes app" });
+// });
+
+const fetchQuotes = async () => {
+  try {
+    const urlHeaders = {
+      headers: { "X-Api-Key": process.env.QUOTES_API_KEY },
+    };
+    const response = await fetch(
+      "https://api.api-ninjas.com/v1/quotes",
+      urlHeaders
+    );
+    const data = response.json();
+    return data;
+  } catch (err) {
+    return err.message;
+  }
+};
+
+router.get("/", async (req, res) => {
+  const data = await fetchQuotes();
+  res.json(data);
 });
 
 module.exports = router;
