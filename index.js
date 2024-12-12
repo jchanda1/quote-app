2bd0fb387e0a75c4cab111570632c9c0915b562e
jchanda1 <74137050+jchanda1@users.noreply.github.com>
2024-12-12 07:43:46 +0000

    add cors policy

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const app = express();
 const port = 3000;
 
 const weather = require("./quotes");
+const { useCallback } = require("react");
 
 app.use(express.json());
 
-app.use(cors());
+const whitelist = ["http://localhost:5173"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("you are not permitted to view this!"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+const limiter = rateLimit({ windowMs: 1000, max: 1 });
+app.use(limiter);
 
 app.get("/", (req, res) => res.json({ Success: "hello worlds" }));
 
